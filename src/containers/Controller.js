fbeca67ec3ee8b59b0abe719b180e650c22fa5fd
patricklinpl <patricklin@alumni.ubc.ca>
2017-10-25 16:06:18 -0700

    template table theme

--- src/containers/Controller.js
+++ src/containers/Controller.js
@@ -1,11 +1,14 @@
 /* global XMLHttpRequest, FormData */
 import React, { Component } from 'react'
 import Form from '../components/Form'
+import PreviewContact from '../components/PreviewContact'
 
 export default class Controller extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      uploadCSV: true,
+      contacts: null
     }
   }
 
@@ -15,14 +18,16 @@ export default class Controller extends Component {
       let FD = new FormData()
       for (let name in state) {
         FD.append(name, state[name])
-        console.log(name)
-        console.log(state[name])
       }
       const xhr = new XMLHttpRequest()
       xhr.onload = () => {
         if (xhr.readyState === 4 && xhr.status === 200) {
           const json = JSON.parse(xhr.response)
-          console.log(json)
+          this.setState({
+            contacts: json,
+            uploadCSV: false,
+          })
+          state.contacts = json
         }
       }
       xhr.open('POST', 'api/form')
@@ -34,7 +39,8 @@ export default class Controller extends Component {
     return (
       <div className='app-container'>
         <h1 style={{ textAlign: 'center' }}> Mass Mailer </h1>
-        <Form handleSubmit={this.handleSubmit.bind(this)} />
+        {this.state.uploadCSV === true ? <Form handleSubmit={this.handleSubmit.bind(this)} /> : null}
+        {typeof this.state.contacts !== null ? <PreviewContact /> : null}
       </div>
     )
   }
