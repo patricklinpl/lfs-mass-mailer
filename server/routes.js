ccdc8083e8ef04237d5dc8f039f6f243173029ea
patricklinpl <patricklin@alumni.ubc.ca>
2017-12-01 15:28:09 -0800

    parse csv without saving, Fix #7

--- server/routes.js
+++ server/routes.js
@@ -1,24 +1,7 @@
 import { Router } from 'express'
-import { userUpload } from './upload'
-import { readCSV } from './parser'
 import { parseData } from './email'
 
 const routes = Router()
-
-routes.post('/api/form', (req, res) => {
-  userUpload(req, res, (error) => {
-    if (error) {
-      console.log(error)
-      res.status(404).send({ type: 'error', msg: error.message })
-    } else {
-      readCSV(req.file).then(data => res.status(200).send({csv: data}))
-      .catch(e => {
-        console.log(e)
-        res.status(404).send({ type: 'error', msg: e.message })
-      })
-    }
-  })
-})
 
 routes.post('/api/send-email', (req, res) => {
   parseData(req.body, (error) => {
